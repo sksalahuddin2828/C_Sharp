e4346a580c835263b17485905262e07d067ac9c4
SK. SALAHUDDIN <127140921+sksalahuddin2828@users.noreply.github.com>
2023-07-15 13:48:07 +0600

    Update palindrome.cs

--- palindrome.cs
+++ palindrome.cs
@@ -16,3 +16,22 @@ class Program {
         }
     }
 }
+
+//------------------------------------------------------------------------------------
+
+using System;
+using System.Linq;
+
+class Program {
+    static bool IsPalindrome(string s) {
+        s = new string(s.Where(char.IsLetterOrDigit).Select(char.ToLower).ToArray());
+        return s == new string(s.Reverse().ToArray());
+    }
+
+    static void Main() {
+        string str = "A man, a plan, a canal: Panama";
+        Console.WriteLine(IsPalindrome(str) ? "True" : "False");  
+    }
+}
+
+// Output: True
