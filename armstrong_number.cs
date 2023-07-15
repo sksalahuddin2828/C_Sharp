87fded0f0e0caee65b1911a712afe784d7840a43
SK. SALAHUDDIN <127140921+sksalahuddin2828@users.noreply.github.com>
2023-07-15 13:35:44 +0600

    Create armstrong_number.cs

---
+++ armstrong_number.cs
@@ -0,0 +1,25 @@
+using System;
+
+class Program {
+    static bool IsArmstrongNumber(int n) {
+        int numDigits = n.ToString().Length;
+        int sumOfPowers = 0;
+        int temp = n;
+        while (temp > 0) {
+            int digit = temp % 10;
+            sumOfPowers += (int)Math.Pow(digit, numDigits);
+            temp /= 10;
+        }
+        return n == sumOfPowers;
+    }
+
+    static void Main() {
+        Console.Write("Enter a number: ");
+        int num = int.Parse(Console.ReadLine());
+        if (IsArmstrongNumber(num)) {
+            Console.WriteLine($"{num} is an Armstrong number.");
+        } else {
+            Console.WriteLine($"{num} is not an Armstrong number.");
+        }
+    }
+}
